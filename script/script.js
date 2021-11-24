c1fce5da1b872e73599528f14ed160dd1d759aa8
zhaziKemelbekova <zhazgulukemelbekova@gmail.com>
2021-11-24 16:33:16 +0400

    js3

---
+++ script/script.js
@@ -0,0 +1,162 @@
+
+// const a = 12
+//  if (a===10){
+//   console.log("true")
+//  }else {
+//   console.log("false")
+//  }
+
+// const a = 50
+// const b = 100
+//
+// if (a > 100){
+//  console.log("false")
+// }else {
+//  console.log("true")
+// }
+
+// const a = -2
+//
+// if (a > 0){
+//  console.log("positive")
+// }else if (a === 0) {
+//  console.log("Equal")
+// }else{
+//  console.log("negative")
+// }
+
+// const num = 45
+//
+// if (num / 2){
+//  console.log("четное")
+// }else {
+//  console.log("нечетное")
+// }
+
+// const num = 45
+//
+// if (num / 5){
+//  console.log("четное")
+// }else {
+//  console.log("нечетное")
+// }
+//
+//
+//
+// const a =10
+// const b = 2
+//
+// if (10 % 2){
+//  console.log("четное")
+// }else {
+//  console.log("нечетное")
+// }
+
+// const s = "flower"
+//
+// switch (typeof s) {
+//     case "string" : console.log("string")
+//         break
+//     case "number" : console.log("number")
+//         break
+//     case "boolean" : console.log("boolean")
+//         break
+// }
+//8
+// const a = 8
+//
+// if (a > 1 && a < 9){
+//     console.log("True")
+// }else{
+//     console.log("false")
+// }
+// const a = 7
+//  if (a === 3 || a ===7){
+//      console.log("true")
+//  }else if ( a + 7){
+//      console.log( "equal")
+//  }else {
+//      console.log("false")
+//  }
+
+// const a = -3
+// const b = 5
+// if (a<=0, b >=5){
+//     console.log("true")
+// }else {
+//     console.log("false")
+// }
+
+// const a = 2
+// const b = 10
+//
+// if (a >4 && a < 10 || b >=7 && b < 17){
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+// const month = 6 {
+//
+// }
+//  switch (month) {
+//      case 1 : cole.log("Зима")
+//         break
+//     case 2 : console.log("Зима")
+//         break
+//     case 3 : console.log("Весна")
+//         break
+//     case 4 : console.log("Весна")
+//         break
+//     case 5 : console.log("Весна")
+//         break
+//     case 6 : console.log("Лето")
+//         break
+//     case 7 : console.log("Лето")
+//         break
+//     case 8 : console.log("Лето")
+//         break
+//     case 9 : console.log("Осень")
+//         break
+//     case 10 : console.log("Осень")
+//         break
+//     case 11 : console.log("Осень")
+//         break
+//     case 12 : console.log("Зима")
+//         break
+// }
+
+// const day = 15
+//
+// if (day<11 && day >10){
+//     console.log("первую")
+// }else if (day<21 && day>10){
+//     console.log("вторую")
+// }else if (day<31 &&day>20){
+//     console.log("третью")
+// }else {
+//     console.log("1 - 31")
+// }
+
+// const age = 30
+// if (age>30) {
+//     console.log("год")
+// }else if (age<30){
+//     console.log(лет)
+// } else {
+//     console.log("мне "+age+"год, мне "+ age+"лет ")
+// }
+
+const time = 18
+if (time<15 && time>16){
+    console.log("first")
+}else if (time>15 && time<30){
+    console.log("second")
+}else if (time>30 && time<45) {
+    console.log("third")
+}else if (time>45 && time<60) {
+    console.log("four")
+}else {
+    console.log("put 1-59")
+}
+
